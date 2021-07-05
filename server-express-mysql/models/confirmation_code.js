967ac453e095ad3955fa1619ab84966beffe014b
Stephan Weber <sdweber422@gmail.com>
2021-07-04 22:26:37 -0700

    adds active field to user model and new confirmation code model

--- server-express-mysql/models/confirmation_code.js
+++ server-express-mysql/models/confirmation_code.js
@@ -3,35 +3,37 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class task extends Model {
+  class confirmation_code extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
   };
-  task.init({
-    id: {
+  confirmation_code.init({
+    code_id: {
       allowNull: false,
       autoIncrement: true,
       primaryKey: true,
       type: DataTypes.INTEGER.UNSIGNED
     },
-    name: DataTypes.STRING,
-    complete: DataTypes.BOOLEAN,
-    createdAt: {
+    confirmation_code: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    created_at: {
       allowNull: false,
       type: DataTypes.DATE,
       defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
     },
-    updatedAt: {
+    expires_at: {
       allowNull: false,
       type: DataTypes.DATE,
       defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
     }
   }, {
     sequelize,
-    modelName: 'task',
+    modelName: 'confirmation_code',
   });
-  return task;
+  return confirmation_code;
 };

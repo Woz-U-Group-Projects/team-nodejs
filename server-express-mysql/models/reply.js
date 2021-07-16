65dcbb1c54d89de05a88ea57c72dcd9bffe8db37
Stephan Weber <sdweber422@gmail.com>
2021-07-16 12:41:01 -0700

    Updates associations on the models

--- server-express-mysql/models/reply.js
+++ server-express-mysql/models/reply.js
@@ -4,11 +4,10 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class reply extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    static associate(models) {
+      models.reply.belongsTo(models.topic, { foreignKey: 'topicId' });
+      models.reply.belongsTo(models.user, { foreignKey: 'userId' });
+    }
   };
   reply.init({
     replyId: {
